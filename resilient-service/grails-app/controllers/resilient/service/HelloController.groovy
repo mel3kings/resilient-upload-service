9e9a23ebe4df690db9bc165ef5fee4859293e8d0
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-02-04 21:20:18 +1100

    convert spring boot java to grails app

---
+++ resilient-service/grails-app/controllers/resilient/service/HelloController.groovy
@@ -0,0 +1,13 @@
+package resilient.service
+
+
+import grails.rest.*
+import grails.converters.*
+
+class HelloController {
+	static responseFormats = ['json', 'xml']
+	
+def index() {
+    render "Hello world!"
+}
+}
---
+++ resilient-service/grails-app/init/resilient/service/BootStrap.groovy
@@ -0,0 +1,9 @@
+package resilient.service
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
